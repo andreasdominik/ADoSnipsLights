4d0964ae9bea91f30b3906711e79f688e07d24a9
andreasdominik <andreas.dominik@mni.thm.de>
2019-05-16 18:08:17 +0200

    Inital commit for new Skill ADoSnipsLights

--- loader-ADoSnipsLights.jl
+++ loader-ADoSnipsLights.jl
@@ -8,8 +8,8 @@
 #
 
 APP_DIR = @__DIR__
-include("$APP_DIR/Skill/ADoSnipsTemplate.jl")
-import Main.ADoSnipsTemplate
+include("$APP_DIR/Skill/ADoSnipsLights.jl")
+import Main.ADoSnipsLights
 
 global INTENT_ACTIONS
-append!(INTENT_ACTIONS, ADoSnipsTemplate.getIntentActions())
+append!(INTENT_ACTIONS, ADoSnipsLights.getIntentActions())
