424443cb24c9fe6ad9ef2a0a1083ddd49ecba92f
andreasdominik <andreas.dominik@mni.thm.de>
2019-05-20 12:13:41 +0200

    intet changed to topic in callback

--- Skill/exported.jl
+++ Skill/exported.jl
@@ -13,12 +13,12 @@ end
 # This function is executed to run a
 # skill action in the module.
 #
-function callbackRun(fun, intent, payload)
+function callbackRun(fun, topic, payload)
 
     Snips.setSiteId(payload[:siteId])
     Snips.setSessionId(payload[:sessionId])
     Snips.setTopic(topic)
-    result = fun(intent, payload)
+    result = fun(topic, payload)
 
     # fix, if the action does not return true or false:
     #
